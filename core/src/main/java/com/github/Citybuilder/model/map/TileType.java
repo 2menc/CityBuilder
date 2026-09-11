b9d7b506a5eeff9b0219c636c715abe3426c2c0a
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-09-11 18:10:19 +0200

    add new materials

--- core/src/main/java/com/github/Citybuilder/model/map/TileType.java
+++ core/src/main/java/com/github/Citybuilder/model/map/TileType.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.graphics.Color;
 
 public enum TileType {
 
-    SELVATIC_GRASS(12, 0, Color.FOREST),
-    GRASS(6, 1, Color.OLIVE),
+    SELVATIC_GRASS(6, 0, Color.FOREST),
+    GRASS(6, 0, Color.OLIVE),
     DIRT(5, 0, Color.BROWN),
-    ROAD(50, 3, Color.GRAY),
-    WATER(25, 0, Color.CYAN);
+    SAND(3, 0, Color.TAN),
+    ROAD(50, 4, Color.DARK_GRAY),
+    CONCRETE(18, 2, Color.GRAY),
+    WOOD(10, 6, new Color(0.65f, 0.50f, 0.25f, 1f)),
+    WATER(15, 0, Color.CYAN);
 
     private final long price;
     private final long pricePerWeek;
@@ -34,15 +37,22 @@ public enum TileType {
 
     public static TileType getTileTypeFromFloat(double elevation, double riverIntensity) {
         
-        if(riverIntensity < 0.03 || elevation < 0.35) {
+        if(riverIntensity < 0.02 || elevation < 0.35) {
             return TileType.WATER;  
         } 
-        
-        if(elevation < 0.42) {
+        if((riverIntensity < 0.045 && riverIntensity >= 0.03) 
+                || (elevation < 0.4 && elevation >= 0.35)) {
+            return TileType.SAND;  
+        }         
+        if(riverIntensity < 0.1 
+                || (elevation > 0.55 && elevation < 0.78)) {
+            return TileType.SELVATIC_GRASS;  
+        }     
+        if(elevation < 0.455 && elevation > 0.4) {
             return TileType.DIRT;
         } 
         
-        return TileType.SELVATIC_GRASS;
+        return TileType.GRASS;
     } 
 
     public String getFormattedName() {
